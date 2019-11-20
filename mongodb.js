14b2066762e03d38477fb474cedc94a0659538d9
Cared24 <ponuszrichard93@gmail.com>
2019-11-20 16:42:21 +0100

    error occured while trying to update + added txt file on 'How to run node'

--- mongodb.js
+++ mongodb.js
@@ -6,15 +6,24 @@ const MongoClient = mongodb.MongoClient;
 const connectionURL = 'mongodb://127.0.0.1:27017';
 const databaseName = 'task-manager';
 
-MongoClient.connect(connectionURL, { useNewUrlParser: true}, (error, client) => {
+MongoClient.connect(connectionURL, { useNewUrlParser: true }, (error, client) => {
     if (error) {
-        return console.log('Unable to connect to database');
+        return console.log('Unable to connect to database :(');
     }
 
     const db = client.db(databaseName);
 
-    db.collection('users').insertOne({
-        name: 'Richard',
-        age: 26
+    const updatePromise = db.collection('users').updateOne({
+        _id: new ObjectID("5dd55aa39dba5d1d0c5caf2c")
+    }, {
+        $set: {
+            name: 'Alicia'
+        }
+    });
+
+    updatePromise.then((result) => {
+        console.log(result);
+    }).catch((error) => {
+        console.log(error);
     })
 })
