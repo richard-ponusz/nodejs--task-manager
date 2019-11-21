63352310813ed3abd6f6e4aaad13e2f6dda0f4e1
Richárd Pónusz <ponuszrichard93@gmail.com>
2019-11-21 11:38:41 +0100

    Added updateMany and comments

--- mongodb.js
+++ mongodb.js
@@ -1,29 +1,46 @@
 // CRUD -> CREATE READ UPDATA DELETE
 
-const mongodb = require('mongodb');
-const MongoClient = mongodb.MongoClient;
+const { MongoClient, ObjectID } = require('mongodb');
 
 const connectionURL = 'mongodb://127.0.0.1:27017';
 const databaseName = 'task-manager';
 
-MongoClient.connect(connectionURL, { useNewUrlParser: true }, (error, client) => {
-    if (error) {
-        return console.log('Unable to connect to database :(');
+MongoClient.connect(connectionURL, { useNewUrlParser: true, useUnifiedTopology: true }, (error, client) => {
+  if (error) {
+    return console.log('Unable to connect to database :(');
+  }
+
+  const db = client.db(databaseName);
+
+  db.collection('users').updateOne({
+    _id: new ObjectID("5dce741ea8fd0c48982a1d7f")
+  }, {
+    /*
+    update upper line's objectId's name property to Alicia
+    $set: {
+      name: 'Alicia'
     }
 
-    const db = client.db(databaseName);
+    Increment age by 1
+    $inc: {
+      age: 1
+    }
+  }).then((result) => {
+    console.log(result);
+  }).catch((error) => {
+    console.log(error);
+  })
+  */
+  })
 
-    const updatePromise = db.collection('users').updateOne({
-        _id: new ObjectID("5dd55aa39dba5d1d0c5caf2c")
-    }, {
-        $set: {
-            name: 'Alicia'
-        }
-    });
-
-    updatePromise.then((result) => {
-        console.log(result);
-    }).catch((error) => {
-        console.log(error);
-    })
-})
+  // in tasks DB set every completed boolean value to true if it was false before 
+  db.collection('tasks').updateMany({
+    'completed': false
+  }, {
+    $set: {
+      completed: true
+    }
+  })
+    .then(result => console.log(result))
+    .catch(error => console.log(error));
+});
