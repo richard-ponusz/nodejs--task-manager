28863de9b263b7227ba6d64b28b581e963781411
Richárd Pónusz <ponuszrichard93@gmail.com>
2019-12-29 21:49:04 +0100

    Added reading and find data from url param

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
-const express = require('express');
 require('./db/mongoose');
+
+const express = require('express');
 const User = require('./models/user');
 const Task = require('./models/task');
 
@@ -8,26 +9,71 @@ const port = process.env.PORT || 3000;
 
 app.use(express.json());
 
+// USER
 app.post(`/users`, (req, res) => {
     const user = new User(req.body);
 
     user.save().then(() => {
-        res.send(user);
+        res.status(201).send(user);
     }).catch((error) => {
         res.status(400).send(error);
     })
 })
 
-app.post('/task', (req, res) => {
-    const task = new Task(req.body);
-
-    task.save().then(() => {
-        res.send(task);
-    }).catch((error) => {
-        res.send(error);
+app.get('/users', (req, res) => {
+    User.find({}).then((users) => {
+        res.send(users)
+    }).catch((e) => {
+        res.status(500).send();
     })
 })
 
+app.get('/users/:id', (req, res) => {
+    const _id = req.params.id;
+
+    User.findById(_id).then((user) => {
+        if (!user) {
+            return res.status(404).send();
+        }
+
+        res.send(user);
+    }).catch((e) => {
+        return res.status(500).send();
+    })
+})
+
+
+// TASK
+app.post('/tasks', (req, res) => {
+    const task = new Task(req.body);
+
+    task.save().then(() => {
+        res.status(201).send(task);
+    }).catch((error) => {
+        res.status(400).send(error);
+    })
+})
+
+app.get('/tasks', (req, res) => {
+    Task.find({}).then((tasks) => {
+        res.send(tasks);
+    }).catch((e) => {
+        res.status(500).send();
+    })
+})
+
+app.get('/tasks/:id', (req, res) => {
+    const _id = req.params.id;
+
+    Task.findById(_id).then((user) => {
+        res.send(user);
+    }).catch((e) => {
+        res.status(404).send();
+    })
+})
+
+
+// PORT
 app.listen(port, () => {
     console.log(`Server is up on port ${port}`);
 })
