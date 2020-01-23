8f01cfa293f72157f7d3749688b8d5fe86e35d4d
Cared24 <ponuszrichard93@gmail.com>
2020-01-23 16:40:09 +0100

    logging in users with email, password is done (105)

--- src/models/user.js
+++ src/models/user.js
@@ -20,6 +20,7 @@ const userSchema = new mongoose.Schema({
     email: {
         type: String,
         required: true,
+        unique: true,
         trim: true,
         lowercase: true,
         validate(value) {
@@ -40,8 +41,25 @@ const userSchema = new mongoose.Schema({
 
         }
     }
-})
+});
 
+userSchema.statics.findByCredentials = async (email, password) => {
+    const user = await User.findOne({ email: email, password: password });
+
+    if (!user) {
+        throw new Error('Unable to login!');
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password);
+
+    if (!isMatch) {
+        throw new Error('Unable to Login!');
+    }
+
+    return user;
+}
+
+// Hash the plaintext password before saving
 userSchema.pre('save', async function (next) {
     const user = this;
 
